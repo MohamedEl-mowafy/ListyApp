8f605a42baf51ff44889afff53d711d485bcd922
MohamedEl-mowafy <80730320+MohamedEl-mowafy@users.noreply.github.com>
2021-04-28 03:02:34 +0200

    finish app

--- CoreMotion/Controller/MainVC.swift
+++ CoreMotion/Controller/MainVC.swift
@@ -23,10 +23,14 @@ class MainVC: UITableViewController {
         return 1
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //Number of Cells
+        return imageArray.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // return a configured cell with image and name.
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ParallaxCell", for: indexPath) as? ParallaxCell
+        else{ return UITableViewCell() }
+        cell.configureCell(withImage: imageArray[indexPath.row], andDescription: nameArray[indexPath.row])
+        return cell 
     }
 
 }
---
+++ CoreMotion/Model/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  CoreMotion
+//
+//  Created by Mohamed Elmowafy on 28/04/2021.
+//
+
+import UIKit
+
+var camera = UIImage(named: "1")!
+var city = UIImage(named: "2")!
+var animals = UIImage(named: "3")!
+var flowers = UIImage(named: "4")!
+var stand = UIImage(named: "5")!
+var urbah = UIImage(named: "6")!
+
+var imageArray = [camera,city,animals,flowers,stand,urbah]
+var nameArray = ["CAMERA","CITY","ANIMALS","FLOWERS","STAND","URBAH"]
